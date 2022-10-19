b696e5f8a948003f19c04241e2365f710e29e5f4
tallner <ctallner@hotmail.com>
2022-10-19 21:40:36 +0200

    temp fragment bugfixed

--- app/src/main/java/com/tallner/apiintegration/CurrentTempFragment.kt
+++ app/src/main/java/com/tallner/apiintegration/CurrentTempFragment.kt
@@ -18,12 +18,9 @@ class CurrentTempFragment : Fragment() {
     private val myFirebaseHelper = FirebaseHelper()
     private lateinit var tvTemperature: TextView
     private lateinit var tvMyCity: TextView
-    private var city = "lund"
-    private var country = "sweden"
-    private var url =
-        "https://api.openweathermap.org/data/2.5/weather?q="+
-                city+ "," + country +
-                "&appid=35ec794bb2d83a735fb4edfd249390a7"
+    private var city = ""
+    private var country = ""
+
 
 
     override fun onCreateView(
@@ -41,48 +38,51 @@ class CurrentTempFragment : Fragment() {
 
             var loggedInUser:MainActivity = context as MainActivity
 
-            myFirebaseHelper.getUserData(loggedInUser.toString(), object : FirebaseHelper.GetUserCallback {
+            myFirebaseHelper.getUserData(loggedInUser.getUsername(), object : FirebaseHelper.GetUserCallback {
                 override fun onCallback(userdata: User) {
                     city = userdata.city
                     country = userdata.country
+
+                    var url =
+                        "https://api.openweathermap.org/data/2.5/weather?q="+
+                                city+ "," + country +
+                                "&appid=35ec794bb2d83a735fb4edfd249390a7"
+
+
+
+
+                    tvMyCity = view.findViewById(R.id.tv_myTemp)
+                    tvMyCity.text = city.uppercase()
+
+                    tvTemperature = view.findViewById(R.id.tv_temperature)
+
+                    // on below line we are creating a variable for our
+                    // request queue and initializing it.
+                    val queue: RequestQueue = Volley.newRequestQueue(view.context)
+
+                    val JsonRequest = JsonObjectRequest(
+                        Request.Method.GET, url, null,
+                        { response ->
+                            try {
+
+                                var tmp = response.getJSONObject("main").getString("temp").toFloat().toInt()
+                                tmp -= 272
+                                tvTemperature.text = tmp.toString() + " °C"
+
+                            }catch (e : Exception){
+                                e.printStackTrace()
+                            }
+                        },
+                        { error ->
+                            tvTemperature.text = "Not available"
+                        })
+                    queue.add(JsonRequest)
+
                     return
                 }
             })
 
-            var url =
-                "https://api.openweathermap.org/data/2.5/weather?q="+
-                        city+ "," + country +
-                        "&appid=35ec794bb2d83a735fb4edfd249390a7"
 
-
-
-
-            tvMyCity = view.findViewById(R.id.tv_myTemp)
-            tvMyCity.text = city.uppercase()
-
-            tvTemperature = view.findViewById(R.id.tv_temperature)
-
-            // on below line we are creating a variable for our
-            // request queue and initializing it.
-            val queue: RequestQueue = Volley.newRequestQueue(view.context)
-
-            val JsonRequest = JsonObjectRequest(
-                Request.Method.GET, url, null,
-                { response ->
-                    try {
-
-                        var tmp = response.getJSONObject("main").getString("temp").toFloat().toInt()
-                        tmp -= 272
-                        tvTemperature.text = tmp.toString() + " °C"
-
-                    }catch (e : Exception){
-                        e.printStackTrace()
-                    }
-                },
-                { error ->
-                    tvTemperature.text = "Not available"
-                })
-            queue.add(JsonRequest)
         }
     }
 }
